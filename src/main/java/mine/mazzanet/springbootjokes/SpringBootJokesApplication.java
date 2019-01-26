bd7c70ef604770a1b68bdcfb5844d42af0398458
mazzac409 <mazzachris.work@gmail.com>
2019-01-25 19:28:13 -0600

    Initial commit.

---
+++ src/main/java/mine/mazzanet/springbootjokes/SpringBootJokesApplication.java
@@ -0,0 +1,14 @@
+package mine.mazzanet.springbootjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJokesApplication.class, args);
+	}
+
+}
+
